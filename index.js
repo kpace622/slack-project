83bb3755dcef3df247c43944f53410f0c1b8bcfd
Kyle Pace <kpace2296@gmail.com>
2020-06-08 21:15:37 -0500

    add env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const server = require('./server.js')
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
