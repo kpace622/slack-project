2ddbf64ac78cbf9c2f2fe67d4641f39af90fd4b1
Kyle Pace <kpace2296@gmail.com>
2020-06-08 21:35:17 -0500

    event!

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ server.post('/api/view', (req, res) => {
         console.log(req.body)
         res.send(req.body.challenge);
     } else {
-        console.log('Slack Payload: ' + req.body.event.text);
+        console.log('Slack Payload: ' + req.body.event);
     }
 })
 
