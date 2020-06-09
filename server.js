e1df3a2a96ebab0fd94be9c5eaaa53a4a5e4dae7
Kyle Pace <kpace2296@gmail.com>
2020-06-08 21:10:01 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send(`
+      <h2>Slack Project!</h>
+    `);
+  });
+
+server.post('/api/view', (req, res) => {
+    console.log('check')
+    if(req.body.challenge) {
+        res.status(200);
+        res.contentType('text/plain');
+        console.log(req.body)
+        res.send(req.body.challenge);
+    } else {
+        console.log('Slack Payload: ' + req.body.dataString);
+    }
+})
+
+module.exports = server;
