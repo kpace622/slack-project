09b1b01f96230c75e7fb5c547b55ae1777cbdaf3
Kyle Pace <kpace2296@gmail.com>
2020-06-12 20:38:39 -0500

    changed db url again

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const server = express();
-const db = require('./knexfile/production');
+const db = require('./knexfile');
 const bodyParser = require('body-parser')
 
 server.use(express.json());
