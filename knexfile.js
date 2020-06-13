2879caa4422719b4213d991bdc574a42fc8ea448
Kyle Pace <kpace2296@gmail.com>
2020-06-12 21:48:02 -0500

    added slack user table

--- knexfile.js
+++ knexfile.js
@@ -12,12 +12,6 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-        // host: 'ec2-34-202-88-122.compute-1.amazonaws.com',
-        // database: 'd745d89pjuobqb',
-        // user: 'zmbcjnizeemnbp',
-        // port: 5432,
-        // password: process.env.PASSWORD
-
     pool: {
       min: 2,
       max: 10
---
+++ migrations/20200612213551_slack_user.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('slack_user', tbl => {
+      tbl.string('slack_user', 255).unique().notNullable();
+      tbl.integer('user_id')
+      .references('users.id')
+      .unsigned()
+      .onDelete('truncate');
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('slack_user')
+};
