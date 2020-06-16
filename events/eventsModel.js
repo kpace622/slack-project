06327567fbab948e1f8b6607591073242b20d6cf
Kyle Pace <kpace2296@gmail.com>
2020-06-15 22:41:48 -0500

    changed console log

--- events/eventsModel.js
+++ events/eventsModel.js
@@ -8,7 +8,7 @@ module.exports = {
 function addEvent(event){
     const {text, type, event_ts: event_timestamp, user: slack_user, team, channel, ts: timestamp} = event;
     const newEvent = {text, type, event_timestamp, slack_user, team, channel, timestamp};
-    console.log('event', newEvent)
+    console.log('event2', newEvent)
     return db('events')
         .insert(newEvent)       
 }
